5e60d524c113d3adf85f33a15c745db9bcaa2088
PemaRekdenDorjee <pemarekdendorjee@gmail.com>
2019-12-09 13:30:50 +0600

    Create dzongkhag.sql

---
+++ dzongkhag.sql
@@ -0,0 +1,29 @@
+
+create table dzongkhag(
+	id int(11)  unsigned auto_increment primary key,
+	dzongkhag varchar(50) not null 
+);
+
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('1','Bumthang');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('2','Chhukha');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('3','Dagana');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('4','Gasa');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('5','Haa');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('6','Lhuentse');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('7','Mongar');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('8','Paro');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('9','Pema Gatshel');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('10','Punakha');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('11','Samdrup Jongkhar');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('12','Samtse');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('13','Sarpang');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('14','Thimphu');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('15','Trashigang');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('16','Trashi Yangtse');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('17','Trongsa');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('18','Tsirang');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('19','Wangdue Phodrang');
+INSERT INTO `dzongkhag` (`id`, `dzongkhag`) VALUES('20','Zhemgang');
+
+
+
